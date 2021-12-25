06f72ce473d38ad70e2002843b865f9712a60dd7
kloud <dgudim@gmail.com>
2021-12-25 17:08:32 +0300

    added coloring to sample shader

--- GameMain/shaders/DiffuseShader.hlsl
+++ GameMain/shaders/DiffuseShader.hlsl
@@ -3,6 +3,7 @@ struct PixelShaderInput {
 	float3 norm : NORMAL;
 	float4 viewPos : POSITION0;
 	float3 light : POSITION2;
+	float4 color : COLOR0;
   /*float4 tangent : TANGENT;
 	float4 texCoord : TEXCOORD;*/
 	//float4 color: COLOR0;
@@ -21,7 +22,7 @@ float4 main(PixelShaderInput input) : SV_TARGET{
 
 	float em = emission;
 
-	float3 col = float3(.8,0.5,0.2);
+	float3 col = input.color.xyz;
 	float dif = dot(-light, n);
 	float spec = max(dot(reflect(light, n), -eye), 0);
 	float shadow = 1;//shadowSoft(p, -LIGHT_DIR, .0625, MAX_DIST, MAX_DIST*.1);
