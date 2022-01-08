0190d09ae46951f535bfa96d1087232474567233
kloud <dgudim@gmail.com>
2022-01-08 16:26:55 +0300

    effects enum for easier access and reverb presets

--- Framework/include/GameEngine/SoundEffect.h
+++ Framework/include/GameEngine/SoundEffect.h
@@ -7,8 +7,26 @@
 #define AL_ALEXT_PROTOTYPES
 #include <al/alext.h>
 #include <al/efx.h>
+#include <al/efx-presets.h>
 
 namespace Game {
+
+    enum Effect {
+        REVERB = AL_EFFECT_REVERB,
+        CHORUS = AL_EFFECT_CHORUS,
+        DISTORTION = AL_EFFECT_DISTORTION,
+        ECHO = AL_EFFECT_ECHO,
+        FLANGER = AL_EFFECT_FLANGER,
+        FREQUENCY_SHIFTER = AL_EFFECT_FREQUENCY_SHIFTER,
+        VOCAL_MORPHER = AL_EFFECT_VOCAL_MORPHER,
+        PITCH_SHIFTER = AL_EFFECT_PITCH_SHIFTER,
+        RING_MODULATOR = AL_EFFECT_RING_MODULATOR,
+        AUTOWAH = AL_EFFECT_AUTOWAH,
+        COMPRESSOR = AL_EFFECT_COMPRESSOR,
+        EQUALIZER = AL_EFFECT_EQUALIZER,
+        EAXREVERB = AL_EFFECT_EAXREVERB
+    };
+
     class SoundEffect : private AlDevice
     {
     public:
@@ -18,7 +36,7 @@ namespace Game {
         void setPropertyi(ALenum property, ALint value);
         void setPropertyfv(ALenum property, const ALfloat* values);
         void setPropertyiv(ALenum property, const ALint* values);
-
+        void setEfxReverbPreset(EFXEAXREVERBPROPERTIES preset);
         ALuint effectID;
         ALuint slotID;
     };
--- Framework/src/Engine/SoundEffect.cpp
+++ Framework/src/Engine/SoundEffect.cpp
@@ -31,4 +31,30 @@ namespace Game {
     void SoundEffect::setPropertyiv(ALenum property, const ALint* values) {
         alEffectiv(effectID, property, values);
     }
+
+    void SoundEffect::setEfxReverbPreset(EFXEAXREVERBPROPERTIES preset) {
+        alEffectf(effectID, AL_EAXREVERB_DENSITY, preset.flDensity);
+        alEffectf(effectID, AL_EAXREVERB_DIFFUSION, preset.flDiffusion);
+        alEffectf(effectID, AL_EAXREVERB_GAIN, preset.flGain);
+        alEffectf(effectID, AL_EAXREVERB_GAINHF, preset.flGainHF);
+        alEffectf(effectID, AL_EAXREVERB_GAINLF, preset.flGainLF);
+        alEffectf(effectID, AL_EAXREVERB_DECAY_TIME, preset.flDecayTime);
+        alEffectf(effectID, AL_EAXREVERB_DECAY_HFRATIO, preset.flDecayHFRatio);
+        alEffectf(effectID, AL_EAXREVERB_DECAY_LFRATIO, preset.flDecayLFRatio);
+        alEffectf(effectID, AL_EAXREVERB_REFLECTIONS_GAIN, preset.flReflectionsGain);
+        alEffectf(effectID, AL_EAXREVERB_REFLECTIONS_DELAY, preset.flReflectionsDelay);
+        alEffectfv(effectID, AL_EAXREVERB_REFLECTIONS_PAN, preset.flReflectionsPan);
+        alEffectf(effectID, AL_EAXREVERB_LATE_REVERB_GAIN, preset.flLateReverbGain);
+        alEffectf(effectID, AL_EAXREVERB_LATE_REVERB_DELAY, preset.flLateReverbDelay);
+        alEffectfv(effectID, AL_EAXREVERB_LATE_REVERB_PAN, preset.flLateReverbPan);
+        alEffectf(effectID, AL_EAXREVERB_ECHO_TIME, preset.flEchoTime);
+        alEffectf(effectID, AL_EAXREVERB_ECHO_DEPTH, preset.flEchoDepth);
+        alEffectf(effectID, AL_EAXREVERB_MODULATION_TIME, preset.flModulationTime);
+        alEffectf(effectID, AL_EAXREVERB_MODULATION_DEPTH, preset.flModulationDepth);
+        alEffectf(effectID, AL_EAXREVERB_AIR_ABSORPTION_GAINHF, preset.flAirAbsorptionGainHF);
+        alEffectf(effectID, AL_EAXREVERB_HFREFERENCE, preset.flHFReference);
+        alEffectf(effectID, AL_EAXREVERB_LFREFERENCE, preset.flLFReference);
+        alEffectf(effectID, AL_EAXREVERB_ROOM_ROLLOFF_FACTOR, preset.flRoomRolloffFactor);
+        alEffecti(effectID, AL_EAXREVERB_DECAY_HFLIMIT, preset.iDecayHFLimit);
+    }
 }
