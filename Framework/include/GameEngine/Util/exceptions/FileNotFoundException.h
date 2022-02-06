cf8ab9498bd83cadbb51ef37f62c8b12b5900c71
kloud <dgudim@gmail.com>
2022-02-06 20:06:53 +0300

    file not found exception filename

--- Framework/include/GameEngine/Util/exceptions/FileNotFoundException.h
+++ Framework/include/GameEngine/Util/exceptions/FileNotFoundException.h
@@ -2,11 +2,17 @@
 
 #include "exception.h"
 
-#define THROW_FILE_NOT_FOUND_EXCEPTION throw Game::FileNotFoundException(__LINE__, __FILE__)
+#define THROW_FILE_NOT_FOUND_EXCEPTION(file_name) throw Game::FileNotFoundException(file_name, __LINE__, __FILE__)
 
 namespace Game {
 	class FileNotFoundException : public Exception {
 	public:
-		FileNotFoundException(int line_, const char* file_) : Exception("File not found!", line_, file_) {}
+		FileNotFoundException(const char* file_name, int line_, const char* file_) : Exception("File not found!", line_, file_) {
+			std::string info{};
+			info.append("File Name: ");
+			info.append(file_name);
+			info.append("\n");
+			SetInfo(info.c_str());
+		}
 	};
 }
