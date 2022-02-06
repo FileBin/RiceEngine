333612e6185065ccac7a0e2a7b01688ebf8c2e5c
kloud <dgudim@gmail.com>
2022-02-06 21:50:44 +0300

    text button template

--- Framework/include/GameEngine/Components/UI/TextButton.h
+++ Framework/include/GameEngine/Components/UI/TextButton.h
@@ -8,33 +8,23 @@ namespace Game::UI {
 	class TextButton : public Button {
 
 	public:
-		TextButton(std::string texture, String text = L"Button", D2D1::ColorF color = D2D1::ColorF::White) {
-			SceneObject& obj = GetSceneObject();
-			ResourceManager& resman = obj.GetScene().GetResourceManager();
-			SetImg(&obj.GetScene().GetRender().CreateTexture(resman.GetString(texture)));
-			this->text = text;
-
-			text_component = new Text();
-			setText(text);
-			setTextColor(color);
-			text_component->PreInit(&obj);
+		TextButton(String text = L"Button", D2D1::ColorF color = D2D1::ColorF::White) : Button() {
+			
 		};
 
 		void setText(String text) {
-			text_component->SetText(text);
+			
 		}
 
 		void setTextColor(D2D1::ColorF color) {
-			text_component->SetColor(color);
+			
 		}
 
 		void Draw(Device* device) {
-			Button::Draw(device);
-			text_component->Draw(device);
+			
 		}
 
 	protected:
 		String text;
-		Text* text_component;
 	};
 }
