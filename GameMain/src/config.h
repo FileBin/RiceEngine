722f25e7c6f0ccedef06cbad7a0fdbc50648b5f3
filebin <alex.mousse9@gmail.com>
2022-08-01 05:09:12 +0300

    game main moved into another  repository

--- GameMain/src/config.h
+++
@@ -1,3 +0,0 @@
-#define USE_HDR
-#define MULTI_RENDER
-#define _DEBUG
--- GameMain/src/pch.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#pragma warning( disable : 4838 4244 4267 4005 )
-
-#include "config.h"
-
-#include <Rice/stdafx.hpp>
