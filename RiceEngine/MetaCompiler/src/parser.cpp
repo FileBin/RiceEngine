899a597ff3437d55b55d5d11404464c38396e917
kloud-sauron <dgudim@gmail.com>
2022-10-09 20:30:45 +0300

    metacompiler json parser done

--- RiceEngine/MetaCompiler/src/parser.cpp
+++ RiceEngine/MetaCompiler/src/parser.cpp
@@ -72,11 +72,9 @@ std::string parseStruct(const nlohmann::json::value_type &item, const std::strin
                 name = current_path + "::" + name;
             }
 
-            built_component += "template <> struct TypeOf<" + name +
-                               "> {\n"
-                               "static Type type() {\n";
+            built_component += "map.insert({typeid(" + name + ").hash_code(), (void *)new Type<" + name;
 
-            built_component += "Type type(Type::Struct, \"" + name + "\");\n" + "std::unique_ptr<Field> field";
+            string constructor_string = ">{Types::Struct, \"" + name + "\"";
 
             for (const auto &[field_index, field] : item["inner"].items()) {
                 if (field["kind"] == "FieldDecl") {
@@ -85,22 +83,16 @@ std::string parseStruct(const nlohmann::json::value_type &item, const std::strin
                     auto ftype_raw = field["type"];
                     string ftype = ftype_raw.contains("desugaredQualType") ? ftype_raw["desugaredQualType"] : ftype_raw["qualType"];
 
-                    built_component += "field.reset(new Field{{Type::";
+                    built_component += ", " + ftype;
 
-                    if (isBuiltIn(ftype)) {
-                        built_component += "BuiltIn";
-                    } else {
-                        built_component += "Struct";
-                    }
+                    constructor_string += ", &" + name + "::" + fname;
 
-                    built_component += ", \"" + ftype + "\"}, \"" + fname + "\"});\n";
-
-                    built_component += "type.fields.push_back(std::move(field));\n";
                 } else if (field["kind"] == "CXXRecordDecl" && field.contains("completeDefinition")) {
                     inner_componenets += parseStruct(field, name);
                 }
             }
-            built_component += "return type;}};";
+
+            built_component += constructor_string + "}});";
         }
     }
 
@@ -119,32 +111,80 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    string compileCommands;
+    string compileCommandsFile;
+
     string sourceFile;
+    string sourceFileAbsPath;
+
+    string headerFile;
+    string headerFileName;
 
     string curr_arg;
 
     bool print_to_console = false;
     for (int i = 0; i < argc; i++) {
         curr_arg = std::string(argv[i]);
-        if (curr_arg.starts_with("file=")) {
-            sourceFile = curr_arg.substr(5);
-        } else if (curr_arg.starts_with("commands_path=")) {
-            compileCommands = curr_arg.substr(15);
+        if (curr_arg.starts_with("header_file_path=")) {
+            headerFile = curr_arg.substr(17);
+            filesystem::path headerFilePath = headerFile;
+            headerFileName = headerFilePath.stem().string();
+        } else if (curr_arg.starts_with("source_file_path=")) {
+            sourceFile = curr_arg.substr(17);
+            filesystem::path sourceFilePath = sourceFile;
+            sourceFileAbsPath = filesystem::absolute(sourceFilePath);
+        } else if (curr_arg.starts_with("compile_commands_path=")) {
+            compileCommandsFile = curr_arg.substr(22);
         } else if (curr_arg == "-p") {
             print_to_console = true;
-        } else {
-            cout << "Arg ignored: " << curr_arg;
         }
     }
 
-    const std::string ast = exec("clang++ -Xclang -ast-dump=json -fsyntax-only -fno-color-diagnostics -Wno-visibility '" + sourceFile + "'");
+    if (!headerFile.length()) {
+        cout << "\n\nPlease set header_file_path\n";
+        exit(1);
+    }
+
+    if (!sourceFile.length()) {
+        cout << "\n\nPlease set source_file_path\n";
+        exit(1);
+    }
+
+    cout << "Running on " << sourceFileAbsPath << "\n";
+
+    string additional_params;
+
+    if (compileCommandsFile.length()) {
+        ifstream compileCommandsStream(compileCommandsFile);
+        json compileCommandsJson = json::parse(compileCommandsStream);
+
+        cout << "Including additonal params:";
+
+        for (const auto &[index, compile_command] : compileCommandsJson.items()) {
+            if (filesystem::absolute(compile_command["file"]) == sourceFileAbsPath) {
+                vector<string> params = split(compile_command["command"], ' ');
+                int index = 0;
+                for (const auto &param : params) {
+                    if (param == "-o") {
+                        break;
+                    }
+                    if (index >= 1) {
+                        additional_params += " " + param;
+                        cout << param << "\n";
+                    }
+                    index++;
+                }
+                break;
+            }
+        }
+    }
+
+    const std::string ast = exec("clang++" + additional_params + " -Xclang -ast-dump -fsyntax-only -fno-color-diagnostics -Wno-visibility '" + headerFile + "'");
 
     auto start = chrono::steady_clock::now();
 
     json data = json::parse(ast);
 
-    string built_component = "#include \"ReflectionHelper.hpp\"\n #include \"" + sourceFile + "\" \nnamespace Meta {";
+    string built_component = "inline void BuildTypes(Meta::TypeMap &map) {";
 
     for (const auto &[item_index, item] : data["inner"].items()) {
         built_component += parseStruct(item, "");
@@ -152,13 +192,15 @@ int main(int argc, char *argv[]) {
 
     built_component += "}\n";
 
-    filesystem::path sourceFilePath = sourceFile;
-
-    ofstream myfile(sourceFilePath.stem().string() + "_meta.hpp");
+    ofstream myfile(headerFileName + "_meta.hpp");
     myfile << built_component;
     myfile.close();
 
-    cout << "Built in: " << chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() << " sec";
+    if (print_to_console) {
+        cout << built_component;
+    }
+
+    cout << "\nBuilt in: " << chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() << " sec\n";
 
     return 0;
 }
