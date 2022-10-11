eb5ab86c2d6beb2bf60dd48164fd715e4b985532
filebin <alex.mousse9@gmail.com>
2022-10-11 18:14:04 +0300

    rapidjson removed

--- RiceEngine/ComponentBuilder/include/ReflectionHelper.hpp
+++ RiceEngine/ComponentBuilder/include/ReflectionHelper.hpp
@@ -2,6 +2,8 @@
 #include <bits/utility.h>
 #include <cstddef>
 #include <cstdint>
+#include <functional>
+#include <iostream>
 #include <map>
 #include <memory>
 #include <string>
@@ -12,8 +14,31 @@
 #include "nameof/nameof.hpp"
 
 namespace Meta {
+
+template <size_t Index = 0,                                                 // start iteration at 0 index
+          typename TTuple,                                                  // the tuple type
+          size_t Size = std::tuple_size_v<std::remove_reference_t<TTuple>>, // tuple size
+          typename TCallable,                                               // the callable to bo invoked for each tuple item
+          typename... TArgs                                                 // other arguments to be passed to the callable
+          >
+void for_each(TTuple &&tuple, TCallable &&callable, TArgs &&...args) {
+    if constexpr (Index < Size) {
+        if constexpr (std::is_assignable_v<bool &, std::invoke_result_t<TCallable &&, TArgs &&..., decltype(std::get<Index>(tuple))>>) {
+            if (!std::invoke(callable, args..., std::get<Index>(tuple)))
+                return;
+        } else {
+            std::invoke(callable, args..., std::get<Index>(tuple));
+        }
+
+        if constexpr (Index + 1 < Size)
+            for_each<Index + 1>(std::forward<TTuple>(tuple), std::forward<TCallable>(callable), std::forward<TArgs>(args)...);
+    }
+}
+
 typedef size_t TypeHash;
 typedef std::map<TypeHash, void *> TypeMap;
+
+template <typename T> struct TypeOf;
 
 enum class Types {
     Struct,
@@ -23,47 +48,44 @@ enum class Types {
 template <typename T, typename... MembersT> class Type;
 
 template <typename T> class Member {
-  private:
+    template <typename R> friend struct TypeOf;
     template <typename R, typename... MembersT> friend class Type;
+
+  private:
+    std::string name;
     T member_pointer;
 
-    Member(T pointer) : member_pointer(pointer) {}
+    Member(std::string name, T pointer) : name(name), member_pointer(pointer) {}
 
   public:
-    constexpr char *getName() { return NAMEOF_MEMBER(member_pointer); }
+    std::string getName() const { return name; }
     constexpr T getMemberPointer() { return member_pointer; }
 };
 
 template <typename T, typename... MembersT> class Type {
+    template <typename R> friend struct TypeOf;
+
   private:
     Types type;
-    typedef std::tuple<MembersT T::*...> tuple_t;
+    typedef std::tuple<Member<MembersT T::*>...> tuple_t;
     tuple_t members;
-    Type() = default;
-    Type(const Type &) = default;
     Type(Type &&) = default;
 
-    Type(Types type, MembersT T::*...members) : type(type), members(std::make_tuple(members...)) {}
-    friend void BuildTypes(TypeMap &map);
-    friend void BuildPrimitiveTypes(TypeMap &map);
+    Type(Types type, Member<MembersT T::*>... members) : type(type), members(std::make_tuple(members...)) {}
 
   public:
     constexpr char *getFullName() { return NAMEOF_TYPE(T); }
     constexpr char *getShortName() { return NAMEOF_SHORT_TYPE(T); }
-    constexpr tuple_t &getMembers() { return members; }
+    tuple_t getMembers() { return members; }
     constexpr size_t getMembersCount() { return std::tuple_size_v<tuple_t>; }
-    template <size_t i> constexpr auto getMemberAt() { return Member(std::get<i>(members)); }
+    template <size_t i> constexpr decltype(auto) getMemberAt() { return std::get<i>(members); }
 };
 
 template <typename T> class Type<T> {
+    template <typename R> friend struct TypeOf;
     Types type;
     Type() = default;
-    Type(const Type &) = default;
-    Type(Type &&) = default;
     Type(Types type) : type(type) {}
-
-    friend void BuildTypes(Meta::TypeMap &map);
-    friend void BuildPrimitiveTypes(Meta::TypeMap &map);
 
   public:
     constexpr char *getFullName() { return NAMEOF_TYPE(T); }
@@ -71,41 +93,32 @@ template <typename T> class Type<T> {
     constexpr size_t getMembersCount() { return 0; }
 };
 
-void BuildTypes(Meta::TypeMap &map);
+#define BUILTIN_GEN_TYPE(b)                                                                                                                                                                                                                              \
+    template <> struct TypeOf<b> {                                                                                                                                                                                                                       \
+        Type<b> type() { return Type<b>{Types::BuiltIn}; }                                                                                                                                                                                               \
+    }
 
-class TypeBuilder {
-    friend const auto &type_of();
-    TypeMap type_map;
+BUILTIN_GEN_TYPE(bool);
+BUILTIN_GEN_TYPE(char);
+BUILTIN_GEN_TYPE(unsigned char);
+BUILTIN_GEN_TYPE(short);
+BUILTIN_GEN_TYPE(unsigned short);
+BUILTIN_GEN_TYPE(int);
+BUILTIN_GEN_TYPE(unsigned int);
+BUILTIN_GEN_TYPE(long);
+BUILTIN_GEN_TYPE(unsigned long);
+BUILTIN_GEN_TYPE(float);
+BUILTIN_GEN_TYPE(double);
+BUILTIN_GEN_TYPE(long double);
+BUILTIN_GEN_TYPE(wchar_t);
 
-  public:
-    TypeBuilder(void (*func)(TypeMap &map)) { func(type_map); }
-};
-
-static TypeBuilder builder = BuildTypes;
-
-template <typename T> const auto &type_of() { return builder.type_map.at(typeid(T).hash_code()); }
-
-inline void BuildPrimitiveTypes(TypeMap &map) {
-#define BUILTIN_GEN_TYPE(b) map.insert({typeid(b).hash_code(), (void *)new Type<b>{Types::BuiltIn}})
-    BUILTIN_GEN_TYPE(bool);
-    BUILTIN_GEN_TYPE(char);
-    BUILTIN_GEN_TYPE(unsigned char);
-    BUILTIN_GEN_TYPE(short);
-    BUILTIN_GEN_TYPE(unsigned short);
-    BUILTIN_GEN_TYPE(int);
-    BUILTIN_GEN_TYPE(unsigned int);
-    BUILTIN_GEN_TYPE(long);
-    BUILTIN_GEN_TYPE(unsigned long);
-    BUILTIN_GEN_TYPE(float);
-    BUILTIN_GEN_TYPE(double);
-    BUILTIN_GEN_TYPE(long double);
-    BUILTIN_GEN_TYPE(wchar_t);
 #undef BUILTIN_GEN_TYPE
-}
 
 } // namespace Meta
 
-/* example
+#define REFLECTABLE __attribute__((annotate("reflectable")))
+
+/*example
 
 struct Foo {
     int i;
@@ -116,8 +129,18 @@ struct Bar {
     Foo foo;
     double dbl;
 };
-inline void Meta::BuildTypes(TypeMap &map) {
-    map.insert({typeid(Foo).hash_code(), (void *)new Type<Foo, int, char>{Types::Struct, &Foo::i, &Foo::ch}});
-    map.insert({typeid(Bar).hash_code(), (void *)new Type<Bar, Foo, double>{Types::Struct, &Bar::foo, &Bar::dbl}});
-}
-*/
+
+template <> struct Meta::TypeOf<Foo> {
+    Type<Foo, int, char> type() { return Type<Foo, int, char>{Types::Struct, {"i", &Foo::i}, {"ch", &Foo::ch}}; }
+};
+
+template <> struct Meta::TypeOf<Bar> {
+    Type<Bar, Foo, double> type() { return Type<Bar, Foo, double>{Types::Struct, {"foo", &Bar::foo}, {"dbl", &Bar::dbl}}; }
+};
+
+int main() {
+    auto type = Meta::TypeOf<Foo>().type();
+    auto members = type.getMembers();
+    Meta::for_each(members, [](const auto &member) { std::cout << member.getName() << "\n"; });
+    return 0;
+}*/
