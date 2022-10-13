33038f5dd5778387fbd4891810a55c6093c592f3
kloud-sauron <dgudim@gmail.com>
2022-10-13 18:17:20 +0300

    update submodule

--- RiceEngine/ComponentBuilder/tests/include/test.cpp
+++ RiceEngine/ComponentBuilder/tests/include/test.cpp
@@ -1,14 +1,16 @@
 #define REFLECTABLE __attribute__((annotate("reflectable")))
+#define NOT_REFLECTABLE __attribute__((annotate("not_reflectable")))
+#define TEST __attribute__((annotate("test")))
 
 #include <string>
 
 struct REFLECTABLE struct111 {
-    int i;
+    TEST TEST TEST TEST int i;
 };
 
 struct REFLECTABLE struct222 {
-    double d;
+    REFLECTABLE double d;
     struct REFLECTABLE struc333 {
-        std::string i;
+        REFLECTABLE std::string i;
     };
 };
